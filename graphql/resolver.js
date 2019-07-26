9ed420221878133315eb86368adf738fb5c13476
Ryan B <klhong124@gmail.com>
2019-07-26 19:26:31 +0800

    Update resolver.js

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-var url = 'mongodb://192.168.1.81:27017/GuardianPRO-DB';
+var url = 'mongodb://localhost:27017/GuardianPRO-DB';
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
 	if (err) throw err;
@@ -85,7 +85,6 @@ module.exports = {
 		return result;
 	},
 	updateUser: async (input) => {
-
 		if (input._id) {
 			input._id = new ObjectId(input._id);
 		}
@@ -93,18 +92,21 @@ module.exports = {
 			return new Promise((resolve) => {
 				MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
 					if (err) throw err;
-					db.db('GuardianPRO-DB').collection('users').updateOne({
-						_id: input._id
-					},{
-						$set: {
-							_id: input._id,
-							name:input.name,
-							password:input.password,
-							gender:input.gender,
-							email:input.email,
-							phone:input.phone
+					db.db('GuardianPRO-DB').collection('users').updateOne(
+						{
+							_id: input._id
+						},
+						{
+							$set: {
+								_id: input._id,
+								name: input.name,
+								password: input.password,
+								gender: input.gender,
+								email: input.email,
+								phone: input.phone
+							}
 						}
-					});
+					);
 					db.db('GuardianPRO-DB').collection('users').findOne(input._id, function(err, res) {
 						if (err) throw err;
 						db.close();
